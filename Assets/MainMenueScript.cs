05646252152a0873fe86f5acbf7ca25ce6173f3c
EbtsamNasser <48641602+EbtsamNasser@users.noreply.github.com>
2019-03-18 15:14:21 +0400

    MainMenue

---
+++ Assets/MainMenueScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenueScript : MonoBehaviour
+{
+  public  Button play;
+   public Dropdown Settings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SettingList();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void OnClickButton()
+    {
+        SceneManager.LoadScene("Playing");
+    }
+
+    void SettingList()
+    {
+        List<string> showList = new List<string>() { "Sound Off", "Sound On" };
+        Settings.AddOptions(showList);
+    }
+}
