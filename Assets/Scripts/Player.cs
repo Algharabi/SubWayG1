cf0233f059062cd96c15c1a068c221b746ca881a
Algharabi <47965203+Algharabi@users.noreply.github.com>
2019-03-18 13:53:18 +0400

    Player Update
    Coins Added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int Health { get; set; }
+    int Ammo;
+    float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (ID == 0)
+        //if (PlayerController.Instance.MyPlayer == null)
+        PlayerController.Instance.MyPlayer = this;
+        //else if (ID == 1)
+        //    PlayerController.Instance.Player2 = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //    transform.Translate(1, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //hit object and died
+        //playercontroller.instance.player = null
+        //destory(gameobject)
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance;
+    public Player MyPlayer { get; set; }
+
+    private Rigidbody rg;
+    public float speed = 10.0F;
+    public float jumpspeed = 10.0F;
+
+    
+
+       
+        //public Player Player2 { get; set; }
+
+        void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(this);
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+
+    void Start()
+    {
+        rg = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+                transform.Translate(1, 0, 0);
+        }
+
+        //private void OnCollisionEnter(Collision collision)
+        //{
+        //    //hit object and died
+        //    //playercontroller.instance.player = null
+        //    //destory(gameobject)
+        //}
+
+        //private void OnTriggerEnter(Collider other)
+        //{
+
+        //}
+
+
+
+
+
+    //float translation = Input.GetAxis("Vertical") * speed;
+    //    float straffe = Input.GetAxis("Horizontal") * speed;
+    //    translation *= Time.deltaTime;
+    //    straffe *= Time.deltaTime;
+
+    //    transform.Translate(straffe, 0, translation);
+
+    //    if (Input.GetKey(KeyCode.Space))
+    //    {
+    //        Vector3 atas = new Vector3(0, 100, 0);
+    //        rg.AddForce(atas * speed, ForceMode.Impulse);
+    //    }
+
+    //    if (Input.GetKeyDown("escape"))
+    //        Cursor.lockState = CursorLockMode.None;
+
+
+
+
+}
