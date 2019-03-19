1a52194a9a6903c22db5dea62fe23d7c9827e66e
abdulazizalhinai <47962369+abdulazizalhinai@users.noreply.github.com>
2019-03-19 09:11:36 +0400

    Environment

---
+++ Assets/cupeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cupeScript : MonoBehaviour
+{
+    public GameObject cupe;
+    Transform player;
+    float timer = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= 0.1f;
+        if (timer <= 0)
+        {
+            Instantiate(cupe, new Vector3(Random.Range(7f, -6f), 1.5f, player.position.z - Random.Range(70,100)),
+                    cupe.transform.rotation);
+            timer = 10;
+        }
+    }
+}
---
+++ Assets/movmentScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movmentScript : MonoBehaviour
+{
+    Transform myRB;
+   // public Transform plane;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Transform>();
+       // plane = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //plane.position = new Vector3(plane.position.x, plane.position.y, plane.position.z - 0.1f);
+        myRB.position = new Vector3(myRB.position.x, myRB.position.y, myRB.position.z - 0.1f);
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            myRB.position = new Vector3(myRB.position.x+0.1f, myRB.position.y, myRB.position.z);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            myRB.position = new Vector3(myRB.position.x-0.1f, myRB.position.y, myRB.position.z);
+        }
+    }
+}
