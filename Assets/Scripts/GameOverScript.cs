45ab3d7d7bb1ec5a47e2749eda22f1b4d53c04bd
asmaalriyamia <33996600+asmaalriyamia@users.noreply.github.com>
2019-03-19 14:24:10 +0400

    GameOver

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverScript : MonoBehaviour
 {
+
+    public Button Replay;
+    public Canvas gameCanvas;
+    bool isGameStarted = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,19 @@ public class GameOverScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if (isGameStarted)
+        //{
+        //    gameCanvas.SetTrigger("Hide");
+        //}
     }
+    public void OnClickButton()
+    {
+       
+        SceneManager.LoadScene("PlayerScene");
+    }
+
+
+
+
+
 }
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    bool IsplayerMove = true;
+    public GameObject PuseMenu;
+    public string PlayerState ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PuseMenu.SetActive(false);
+        PlayerState = "Mid";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (IsplayerMove)
+        {
+            transform.Translate(Vector3.forward* Time.deltaTime * 10);
+
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (PlayerState == "Mid")
+                {
+                    PlayerState = "left";
+                    this.transform.position = new Vector3(-7.5f, transform.position.y,transform.position.z);
+                }
+                else if (PlayerState == "right")
+                {
+                    PlayerState = "Mid";
+                    this.transform.position = new Vector3(0f, transform.position.y, transform.position.z);
+                }
+                else
+                {
+                    print("The Player is In left Now");
+                }
+
+            }
+
+
+
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (PlayerState == "Mid")
+                {
+                    PlayerState = "right";
+                    this.transform.position = new Vector3(7.5f, transform.position.y, transform.position.z);
+                }
+                else if (PlayerState == "left")
+                {
+                    PlayerState = "Mid";
+                    this.transform.position = new Vector3(0f, transform.position.y, transform.position.z);
+                }
+                else
+                {
+                    print("The Player is In right Now");
+                }
+
+            }
+
+
+
+
+
+        }
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Coins")
+        {
+            print("takecoins");
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Block")
+        {
+            IsplayerMove = false;
+            PuseMenu.SetActive(true);
+        }
+    }
+
+
+
+
+}
